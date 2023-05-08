b00a2786f848698be4008ab0e9dd6d86c0765f93
Firmino Massango <firminorafael0@gmail.com>
2023-05-08 12:08:43 +0200

    generating an executable file

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from get_users import get_usernames_from_authorized_keys
 import utils
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import PhotoImage
 
-global label
 
-def add_key(user_listbox):
+def add_key():
     # Get the path to the public key file
     pub_key = filedialog.askopenfilename(title="Select Public Key", filetypes=[("Chave pública", "*.pub")])
     if not pub_key:
@@ -15,12 +15,12 @@ def add_key(user_listbox):
     # Call the function to append the key to authorized_keys file
     message = append_key_to_authorized_keys(pub_key, utils.authorized_keys)
     
-    update_message(label, message)
+    update_message(message)
 
     # Update the list of users
     update_user_list(user_listbox)
 
-def update_message(label,message):
+def update_message(message):
     label.config(text=message)
 
 
@@ -35,36 +35,35 @@ def update_user_list(user_listbox):
     for user in users:
         user_listbox.insert(tk.END, user)
 
+# Create the main window
+root = tk.Tk()
+root.title("Intellica - Utilizador Git")
+icon = PhotoImage(file = 'img/icon.png')
+# Icon set for program window
+root.iconphoto(False, icon)
+# Set the default window size
+root.geometry("500x500")
 
-def main():
-    # Create the main window
-    root = tk.Tk()
-    root.title("Intellica - Utilizador Git")
+# Set the background color to white
+root.configure(bg="#FFFFFF")
 
-    # Set the default window size
-    root.geometry("500x500")
+# Add the "Add Public Key" button
+add_key_button = tk.Button(root, text="Adicionar chave pública", bg="#0C8CE9", fg="#FFFFFF", command=add_key)
+add_key_button.pack(padx=10, pady=20)
 
-    # Set the background color to white
-    root.configure(bg="#FFFFFF")
-
-    # Add the "Add Public Key" button
-    add_key_button = tk.Button(root, text="Adicionar chave pública", bg="#0C8CE9", fg="#FFFFFF", command=lambda: add_key(user_listbox))
-    add_key_button.pack(padx=10, pady=20)
-
-    # Add a label to display messages
-    label = tk.Label(root, text="", bg="#FFFFFF", fg="#FF0000")
-    label.pack()
+# Add a label to display messages
+label = tk.Label(root, text="", bg="#FFFFFF", fg="#FF0000")
+label.pack()
 
 
-    # Add the listbox to display the users
-    user_listbox = tk.Listbox(root, bg="#EFEFEF", fg="#4C4C4C")
-    user_listbox.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+# Add the listbox to display the users
+user_listbox = tk.Listbox(root, bg="#EFEFEF", fg="#4C4C4C")
+user_listbox.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-    # Add the users to the listbox
-    update_user_list(user_listbox)
+# Add the users to the listbox
+update_user_list(user_listbox)
 
-    # Start the main event loop
-    root.mainloop()
+# Start the main event loop
+root.mainloop()
 
-if __name__ == "__main__":
-    main()
+
